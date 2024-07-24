c8e643d68126a96775d46fd51bcae5e25e3e05f2
caiobolive <caiobolive@gmail.com>
2024-07-24 15:22:42 -0300

    feat: renaming component with custom library name

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
   onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+const LmButton: React.FC<ButtonProps> = ({ label, onClick }) => {
   return (
     <button className={styles.button} onClick={onClick}>
       {label}
@@ -14,4 +14,4 @@ const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
   );
 };
 
-export default Button;
+export default LmButton;
